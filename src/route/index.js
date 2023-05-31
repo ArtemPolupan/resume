ffe4f6029246d2e2e8754bddce374e1527b696fb
ArtemPolupan <polupanartem342@gmail.com>
2023-05-31 20:31:29 +0300

    BootstrapNew

--- src/route/index.js
+++ src/route/index.js
@@ -1318,5 +1318,20 @@ router.get('/task22', function (req, res) {
 })
 
 // ================================================================
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/bootstrapNew', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrapNew', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
